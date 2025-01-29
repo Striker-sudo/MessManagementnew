905aeb3945f2dbd3f6768dcd392a86033984c7d8
Shubhranshu Mishra <helloworld40444@gmail.com>
2025-01-29 16:09:54 +0530

    deleted Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import LoginPage from "./Login.jsx";
-import MyComponent from "./Button.jsx";
-import Signup from "./Signup.jsx";
+// import LoginPage from "./Login.jsx";
+// import Signup from "./Signup.jsx";
 import Home from "./home.jsx";
-import OTPPage from "./OTP.jsx";
+// import OTPPage from "./OTP.jsx";
 
 function App(){
     return(
